4513709bbdbcdb37ea3cd69e3a3d2b6a0495dc3c
yohaydy <oskars.rukmans@gmail.com>
2020-02-23 19:20:23 +0000

    yes yes yes

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const express = require('express')
-const shell = require('shelljs')
-const app = express()
-const port = 3000
+const express = require("express");
+const shell = require("shelljs");
+const app = express();
+const port = 3000;
 
-app.get('/', async (req, res) => {
-    const fileName = await shell.exec('youtube-dl -x --audio-format mp3 --get-filename https://www.youtube.com/watch?v=WEx7ZlF2FGc', {silent:true}).stdout; 
-    await shell.exec('youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=WEx7ZlF2FGc', {silent:true}).stdout;
-    const fileNameMp3 = fileName.slice(0, -5).concat("mp3")
-    
-    const file = `./${fileNameMp3}`;
-    res.download(file); 
-})
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const fileName = await shell.exec(
+    `youtube-dl -x --audio-format mp3 --get-filename https://www.youtube.com/watch?v=${id}`
+  ).stdout;
+  await shell.exec(
+    `youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=${id}`
+  );
+  const fileNameMp3 = fileName.slice(0, -5).concat(".mp3");
+  const filePath = `./${fileNameMp3}`;
+  res.download(filePath);
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
